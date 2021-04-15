66e3332aaeecc1856f5edcd5a247db77cfa5efc5
Chanwoong <mcw0219@gmail.com>
2021-04-15 22:32:55 +0900

    [Complete betweenTwoIntegers]

---
+++ programmers/betweenTwoIntegers.js
@@ -0,0 +1,42 @@
+/* *******************************
+ * 2021-04-15
+ * 프로그래머스
+ * 두 정수 사이의 합
+ *********************************
+
+ * 문제 설명
+두 정수 a, b가 주어졌을 때 a와 b 사이에 속한 모든 정수의 합을 리턴하는 함수, solution을 완성하세요.
+예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다.
+
+ * 제한 조건
+a와 b가 같은 경우는 둘 중 아무 수나 리턴하세요.
+a와 b는 -10,000,000 이상 10,000,000 이하인 정수입니다.
+a와 b의 대소관계는 정해져있지 않습니다.
+
+ ****************************** */
+//	idea : 
+
+/* SOLUTION */
+function solution(a, b) {
+    var answer = 0;
+    var bigger = Math.max(a, b);
+    var smaller = Math.min(a, b);
+    
+    while(smaller < bigger){
+        answer += smaller;
+        smaller++;
+    }
+    return answer + bigger;
+}
+
+/* TESTCASE & EXECUTE */
+var a = [3,3,5];
+var b = [5,3,3];
+var resultList = [12,3,12]
+
+var i=0;
+while(i < resultList.length){
+	var result = solution(a[i], b[i]);
+	result === resultList[i] ? console.log(`${i}. [Correct]`) : console.log(`${i}. ---Fail---  your result : ${result}`);
+	i++;
+}
